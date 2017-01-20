4c84361be934ebb35cf9b41c46137e97d7075a68
Mike Milonakis <mmilonakis@atypon.com>
2017-01-20 09:27:59 +0200

    Set up the action bar, define options (Tutorial #7)

--- app/src/main/java/com/mooo/sestus/eddystonescanner/MainActivity.java
+++ app/src/main/java/com/mooo/sestus/eddystonescanner/MainActivity.java
@@ -2,6 +2,8 @@ package com.mooo.sestus.eddystonescanner;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,25 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options, menu);
+
+        return(super.onCreateOptionsMenu(menu));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.about:
+                return (true);
+
+            case R.id.help:
+                return (true);
+        }
+
+        return(super.onOptionsItemSelected(item));
     }
 }
